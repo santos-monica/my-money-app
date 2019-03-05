a69e1e2a0292ff248fd3a527ee376bb42224fe9c
Mônica <monica.santos@itlab.com.br>
2019-03-04 22:24:26 -0300

    footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const footer = (props) => {
+  return (
+    <footer className='main-footer'>
+      <strong>
+        Copyright &copy; 2017 
+        <a href="https://github.com/santos-monica" target='blank'> GitHub</a>
+      </strong>
+    </footer>
+  )
+}
+
+export default footer
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
   <div className='wrapper'>
     <Header />
     <Sidebar />
+    <div>
+      <h1>Content</h1>
+    </div>
+    <Footer />
   </div>
 )
