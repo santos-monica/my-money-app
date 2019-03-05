4af611c32be82f7a4693d53377c1f09d821ce9bd
Mônica <monica.santos@itlab.com.br>
2019-03-04 22:51:01 -0300

    Padronizacao do dashboard

---
+++ frontend/src/common/template/content.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Content = (props) => {
+  return (
+    <section className="content">{props.children}</section>
+  )
+}
+
+export default Content
--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Dashboard = (props) => {
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Dashboard' small='Versão 1.0' />
+        <Content>
+          Dashboard
+        </Content>
+      </div>
+    )
+  }
 }
 
 export default Dashboard
